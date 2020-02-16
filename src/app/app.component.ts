a84c17e109114df7761183fd5b0c619334b8a9f7
BennyBen54 <bennyben54@gmail.com>
2020-02-16 17:30:55 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'beo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'beo-games';
+}
